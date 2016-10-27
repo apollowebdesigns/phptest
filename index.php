4d6cbfa227e1155cb78d8adefda288ed030f0134
Andy Evans <andrewevans0@outlook.com>
2016-10-27 21:19:06 +0100

    php changes

--- index.php
+++ index.php
@@ -4,17 +4,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css" />
     <link rel="stylesheet" href="styles.css">
   </head>
-  <body class="container bodyimage">
-    <h1>Hello Mamp</h1>
-    <?php
-        $dir = new DirectoryIterator(dirname(__FILE__));
-        foreach ($dir as $fileinfo) {
-            if (!$fileinfo->isDot()) {
-                echo ($fileinfo->getFilename());
-                echo ("<br>");
-            }
-        }
+  <body class="container row bodyimage">
+    <h1>Andy's working directory</h1>
+    <div class="col s6">
+        <div class="collection">
+            <?php
+                $dir = new DirectoryIterator(dirname(__FILE__));
+                foreach ($dir as $fileinfo) {
+                    if (!$fileinfo->isDot()) {
+                        echo ("<a class='collection-item' href=");
+                        echo ($fileinfo->getFilename());
+                        echo (">");
+                        echo ($fileinfo->getFilename());
+                        echo ("</a>");
+                    }
+                }
+            ?>
+        </div>
+    </div>
+    <div class="col s6">
         
-    ?>
+    </div>
+
   </body>
 </html?
